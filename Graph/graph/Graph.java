06de4a71cdcd0451426d4f795a0a07cc5ddf191e
Bikash <bikash11roy@gmail.com>
2016-07-23 15:54:25 +0600

    Bellman ford

--- Graph/graph/Graph.java
+++ Graph/graph/Graph.java
@@ -1,5 +1,24 @@
 package graph;
 
 public class Graph {
-
+	public class Edge{
+		public int src, dest, weight;
+		
+		public Edge(){
+			src=dest=weight=0;
+		}
+	}
+	
+	public int V;
+	public int E;
+	public Edge edge[];
+	
+	public Graph(int v, int e){
+		V=v;
+		E=e;
+		edge=new Edge[E];
+		for(int i=0; i<e; i++)
+			edge[i]=new Edge();
+	}
+	
 }
