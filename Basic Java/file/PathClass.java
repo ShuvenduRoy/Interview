2c6c621e28c97df786b3477f745b1c79172692a0
bikash <bikash@bikash-desktop>
2016-08-01 10:11:31 +0600

    file in java

---
+++ Basic Java/file/PathClass.java
@@ -0,0 +1,11 @@
+package file;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathClass {
+	public static void main(String[] args){
+		Path path = Paths.get("loremipsun.txt");
+		System.out.println(path.toString());
+	}
+}
