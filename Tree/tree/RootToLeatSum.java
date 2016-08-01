bd633ddb96da3d3c5838a5e6476a12cb27f9ba95
bikash <bikash@bikash-desktop>
2016-08-01 17:42:49 +0600

    Binary tree sum

--- Tree/tree/RootToLeatSum.java
+++ Tree/tree/RootToLeatSum.java
@@ -7,6 +7,20 @@ public class RootToLeatSum extends BinarySearchTree{
 	public boolean printPath(Node root, int sum, List<Node> path){
 		if(root==null)
 			return false;
+		
+		if(root.left==null && root.right==null){
+			if(root.data==sum){
+				path.add(root);
+				return true;
+			}else{
+				return false;
+			}
+		}
+		if(printPath(root.left, sum-root.data, path) || printPath(root.right, sum-root.data, path)){
+			path.add(root);
+			return true;
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
@@ -23,7 +37,7 @@ public class RootToLeatSum extends BinarySearchTree{
         List<Node> result = new ArrayList<>();
         boolean r = bt.printPath(head, 22, result);
         if(r){
-        	
+        	result.forEach(node -> System.out.print(node.data+" "));
         }else{
         	System.out.println("There is no path for sum 22");
         }
