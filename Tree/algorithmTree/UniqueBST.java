ef51d429ffc3578009f3adfad5d1b677d0d17d4a
bikash <bikash@bikash-HP-ProBook-450-G2>
2016-08-10 22:58:59 +0600

    unique BST

--- Tree/algorithmTree/UniqueBST.java
+++ Tree/algorithmTree/UniqueBST.java
@@ -1,6 +1,12 @@
 package algorithmTree;
 
 public class UniqueBST {
+	
+	/* 
+	 * 
+	 * Got TLE in the last input, that is 19
+	 * 
+	 * 
 	public int[] dp;
 	
 	public int tree(int n){
@@ -19,6 +25,19 @@ public class UniqueBST {
 	public int numTrees(int n){
 		dp = new int[n+1];
 		return tree(n);
+	} 
+	
+	*/
+	
+	public int numTrees(int n){
+		int []dp = new int[n+1];
+		dp[0] = dp[1] = 1;
+		for(int i=2; i<=n; i++){
+			for(int j=0; j<i; j++){
+				dp[i]+=dp[j]*dp[i-j-1];
+			}
+		}
+		return dp[n];
 	}
 	
 	public static void main(String []args){
--- Tree/leetcode96/Solution.java
+++ Tree/leetcode96/Solution.java
@@ -1,23 +1,14 @@
 package leetcode96;
 
 public class Solution {
-	public int[] dp;
-	
-	public int tree(int n){
-		if(n<=1)
-			return 1;
-		if(dp[n]!= 0)
-			return dp[n];
-		
-		int total =0;
-		for(int i=0; i<n; i++){
-			total += tree(i) * tree(n-i-1);
-		}
-		return total;
-	}
-	
 	public int numTrees(int n){
-		dp = new int[n+1];
-		return tree(n);
+		int []dp = new int[n+1];
+		dp[0] = dp[1] = 1;
+		for(int i=2; i<=n; i++){
+			for(int j=0; j<i; j++){
+				dp[i]+=dp[j]*dp[i-j-1];
+			}
+		}
+		return dp[n];
 	}
 }
