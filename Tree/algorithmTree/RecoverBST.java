598aff45a2c13e943efb275f2bdf4cf7f3f142e3
bikash <bikash@bikash-HP-ProBook-450-G2>
2016-08-13 17:31:15 +0600

    Recover tree

---
+++ Tree/algorithmTree/RecoverBST.java
@@ -0,0 +1,39 @@
+package algorithmTree;
+
+public class RecoverBST {
+	TreeNode node1 = null;
+	TreeNode node2 = null;
+	TreeNode Pre = null;
+	
+	public void helper(TreeNode root){
+		if(root == null)
+			return;
+		
+		helper(root.left);
+		
+		if(Pre!=null && Pre.val>root.val){
+			if(node1 == null){
+				node1 = Pre;
+			}
+			node2 = root;
+		}
+		
+		Pre = root;
+		helper(root.right);
+	}
+	
+	public void recoverTree(TreeNode root){
+		if(root ==null){
+			return;
+		}
+		
+		helper(root);
+		
+		if(node1!=null && node2!=null){
+			TreeNode temp  = node1;
+			node1=node2;
+			node2= temp;
+		}
+	}
+	
+}
