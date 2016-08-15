6c76245a538d83913195d06a3c2d2c578010d599
bikash <bikash@bikash-HP-ProBook-450-G2>
2016-08-16 04:09:56 +0600

    Bubble sort

--- Searching-Sorting/BubbleSort/BubbleSort.cpp
+++ Searching-Sorting/BubbleSort/BubbleSort.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void Bubble_Sort(int A[],int n)
 {
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n-1; i++)
     {
-        for(int j=0; j<n; j++)
+        for(int j=0; j<n-i-1; j++)
         {
             if(A[j]>A[j+1])
             {
