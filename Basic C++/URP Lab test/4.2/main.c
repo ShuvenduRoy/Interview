39288a733bbb1bc0cffbfdccfbfd90ebd692d057
Bikash <bikash11roy@gmail.com>
2016-08-20 13:01:35 +0600

    urp
    Signed-off-by: Bikash <bikash11roy@gmail.com>

---
+++ Basic C++/URP Lab test/4.2/main.c
@@ -0,0 +1,37 @@
+/**
+* Created By Shuvendu Roy BIkash
+* Date 20 Aug 2016
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+
+int main()
+{
+    char name[50];
+    gets(name);
+
+    int length = strlen(name); //function strlen() is definde in header string.h
+
+    int i;
+    for(i=0; i<length; i++)
+    {
+        if(name[i]>=48 && name[i]<=56)  //In ASCII value 0==48 to 9==57
+            printf("%c",name[i]+1);
+        if(name[i] == 57)
+            printf("0");
+
+        if(name[i]>=65 && name[i]<=89)  //In ASCII value A==65 to Z==90
+            printf("%c",name[i]+1);
+        if(name[i] == 90)
+            printf("A");
+
+            if(name[i]>=97 && name[i]<=121)  //In ASCII value a=97 to z=122
+            printf("%c",name[i]+1);
+        if(name[i] == 122)
+            printf("a");
+
+    }
+}
