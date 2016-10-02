c5b45865054c3f674268355ae2b96f2d0f93ec34
Bikash <bikash11roy@gmail.com>
2016-10-02 22:44:58 +0600

    kruskal's algorithm

---
+++ Graph/graph/Kruskal's algorithm/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+struct Edge
+{
+    int src, dest, weight;
+};
+
+struct
+{
+    int V, E;
+    Edge* edge;
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
