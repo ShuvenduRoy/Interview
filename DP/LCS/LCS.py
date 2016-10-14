ddaa76ae5b294fd1d140d1ab9695a8377768461c
Bikash <bikash11roy@gmail.com>
2016-10-15 02:48:49 +0600

    lcs.py

---
+++ DP/LCS/LCS.py
@@ -0,0 +1,20 @@
+def LCS(x,y):
+    n = len(x)
+    m = len(y)
+
+    L = [[None]*(m+1) for i in range(n+1)]
+
+    for i in range(n+1):
+        for j in range(m+1):
+            if i==0 or j==0:
+                L[i][j] = 0
+            elif x[i-1]==y[j-1]:
+                L[i][j] = L[i-1][j-1] + 1
+            else:
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+
+    return L[n][m]
+
+X = "AGGTAB"
+Y = "GXTXAYB"
+print ( "Length of LCS is ", LCS(X, Y))
