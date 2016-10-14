c734442b0ce5f01737884682b2a3f5dfaeeaaba8
Bikash <bikash11roy@gmail.com>
2016-10-15 02:35:41 +0600

    LCS Renaming

---
+++ DP/LCS/LCS.java
@@ -0,0 +1,32 @@
+package LCS;
+
+public class LCS {
+	
+	int lcsDynamic(char str1[], char str2[]){
+		int m=str1.length;
+		int n=str2.length;
+		int Table[][]=new int[str1.length+1][str2.length+1];
+		
+		for(int i=1; i<=m; i++){
+			for(int j=1; j<=n; j++){
+				if(str1[i-1]==str2[j-1]){
+					Table[i][j]=Table[i-1][j-1]+1;
+				}
+				else 
+					Table[i][j]=Math.max(Table[i-1][j],Table[i][j-1]);
+			}
+		}
+		
+		return Table[m][n];
+	}
+	
+	//Main Function to Test
+	public static void main(String args[]){
+        LCS lcs = new LCS();
+        String str1 = "ABCDGHLQR";
+        String str2 = "AEDPHR";
+
+        int result = lcs.lcsDynamic(str1.toCharArray(), str2.toCharArray());
+        System.out.print(result);
+    }
+}
