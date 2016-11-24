754643c5ac5459c48d8ff995ce9d1cd607aff142
Bikash <bikash11roy@gmail.com>
2016-11-25 05:10:25 +0600

    Matrix Rectangle

--- DP/Matrix Rectangle.py
+++ DP/Matrix Rectangle.py
@@ -1,38 +1,34 @@
+def calc(ans, a, b):
+    temp = (a ) * (b )
+    if temp > ans:
+        ans = temp
+    return ans
+
+
 class Solution(object):
     def maximalRectangle(self, matrix):
         m = len(matrix)
-        if m==0:
+        if m == 0:
             return 0
         n = len(matrix[0])
 
-        row = [[0 for j in range(n)] for i in range(m)]
-        col = [[0 for j in range(n)] for i in range(m)]
-
-        ans = 0
-        if matrix[0][0] == 1:
-            ans = 1
-
-        for i in range(1,m):
-            if matrix[i][0] == 1:
-                row[i][0] = row[i-1][0] + 1
-            else:
-                row[i][0] = 0
-
-        for j in range(1,n):
-            if matrix[0][i] == 1:
-                col[0][i] = col[0][i-1] + 1
-            else:
-                col[0][i] = 0
-
+        dp = [[0 for j in range(n)] for i in range(m)]
+        for i in range(m):
+            dp[i][0] = int(matrix[i][0])
+        for i in range(n):
+            dp[0][i] = int(matrix[0][i])
 
         for i in range(1,m):
             for j in range(1,n):
-                if matrix[i][j]==0:
-                    row[i][j]=0
-                    col[i][j]=0
+                if matrix[i][j] == '1':
+                    dp[i][j] = min(int(matrix[i-1][j]),
+                                       int(matrix[i][j-1]),
+                                           int(matrix[i-1][j-1]))+1
                 else:
-                    if matrix[i-1][j] == matrix[i][-1]==1:
-                        row[i][j] = row[i-1][j] + 1
-                        col[i][j] = col[i][j-1]+1
+                    dp[i][j] = 0
+        return dp[i][j]
 
 
+test = ["10100","10111","11111","10010"]
+s = Solution()
+print(s.maximalRectangle(test))
