531354266c9adcb5fe33be9db15747fcb499fe07
Bikash <bikash11roy@gmail.com>
2016-12-20 16:45:30 +0600

    heapq

--- Divide and Conquare/Leetcode 23. Mege k Sorted List.py
+++ Divide and Conquare/Leetcode 23. Mege k Sorted List.py
@@ -7,3 +7,16 @@ class ListNode(object):
 class Solution(object):
     def mergeKLists(self, lists):
         from heapq import heapify,heappop,heappush, heapreplace
+        dummy = node = ListNode(0)
+
+        h = [(n.val, n) for n in lists if n]
+        heapify(h)
+
+        while h:
+            v,n = h[0]
+            if n.next is None:
+                heappop(h)
+            else:
+                heapreplace(h, (n.next.val, n.next))
+            node.next = n
+            node = node.next
