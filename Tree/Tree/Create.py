9682a22e228134d12960869d8270d804e7915c81
Bikash <bikash11roy@gmail.com>
2017-05-14 05:37:54 +0600

    Constructing BST

---
+++ Tree/Tree/Create.py
@@ -0,0 +1,42 @@
+class Node:
+    val = None
+    LeftNode = None
+    RightNode = None
+
+    def __init__(self, val):
+        self.val = val
+
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def create(self, arr):
+        for val in arr:
+            if self.root == None:
+                self.root = Node(val)
+
+            else:
+                current = self.root
+
+                while 1:
+                    if val<current.val:
+                        if current.LeftNode:
+                            current = current.LeftNode
+                        else:
+                            current.LeftNode = Node(val)
+                            break;
+
+                    else:
+                        if current.RightNode:
+                            current = current.RightNode
+                        else:
+                            current.RightNode = Node(val)
+                            break;
+
+
+arr = [4,3]
+bst = BST()
+bst.create(arr)
+print(bst)
