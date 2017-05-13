9785fc4bd9fde3e7a7d13c5f405c838f2d9ca3b5
Bikash <bikash11roy@gmail.com>
2017-05-14 05:59:14 +0600

    Inorder, preorder, postorder traversal

--- Tree/Tree/TreeTraversal.py
+++ Tree/Tree/TreeTraversal.py
@@ -1,7 +1,35 @@
 from Tree.Tree.Create import BST,Node
 
 
+def InOrder(node):
+    if node is not None:
+        InOrder(node.LeftNode)
+        print(node.val, end=" ")
+        InOrder(node.RightNode)
 
-arr = [4,3]
+
+def preorder(node):
+    if node is not None:
+        print(node.val, end=" ")
+        preorder(node.LeftNode)
+        preorder(node.RightNode)
+
+
+def postorder(node):
+    if node is not None:
+        postorder(node.LeftNode)
+        postorder(node.RightNode)
+        print(node.val, end=" ")
+
+arr = [8,3,1,6,4,7,10,14,13]
 bst = BST()
 bst.create(arr)
+
+print("In Order traversal : ")
+InOrder(bst.root)
+
+print("\nPre Order traversal : ")
+preorder(bst.root)
+
+print("\nPost Order traversal : ")
+postorder(bst.root)
