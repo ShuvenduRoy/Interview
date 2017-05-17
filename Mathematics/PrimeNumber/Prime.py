a0e60e0cab2ccbd133380be65c5f2395f6d9fc5d
Bikash <bikash11roy@gmail.com>
2017-05-17 16:56:36 +0600

    SieveOfEratosthenes of O(n) time

--- Mathematics/PrimeNumber/Prime.py
+++ Mathematics/PrimeNumber/Prime.py
@@ -7,9 +7,10 @@ class Prime:
 
 
     def GetPrimeSmallerThanN(self, n):
-        primes = self.SieveOfEratosthenes(n)
+        primes = self.SieveOfEratosthenes2(n)
         return primes
 
+    # Complexity O(n log(log n) )
     def SieveOfEratosthenes(self, n):
         primes = [True for i in range(n+1)]
         # initialize first prime number
@@ -30,7 +31,28 @@ class Prime:
 
         return ans
 
+    # Complexity of O(n)
+    def SieveOfEratosthenes2(self, n):
+        SPF = [0 for i in range(n+1)]
+        isPrime = [True for i in range(n+1)]
+        isPrime[0] = isPrime[1] = False
+
+        primes = []
+
+        for i in range (2, n+1):
+            if isPrime[i]:
+                primes.append(i)
+                SPF[i] = i
+
+            j = 0
+            while j<len(primes) and i*primes[j]<=n and primes[j] <= SPF[i]:
+                isPrime[primes[j] * i] = False
+                SPF[i * primes[j]] = primes[j]
+                j+=1
+
+        return primes
+
 
 test = Prime()
-primes = test.GetPrimeSmallerThanN(10)
+primes = test.GetPrimeSmallerThanN(100)
 print(primes)
