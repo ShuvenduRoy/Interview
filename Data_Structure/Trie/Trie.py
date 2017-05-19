ee45aa2242c13cf84742ac7129945e69c3c1b77a
Bikash <bikash11roy@gmail.com>
2017-05-19 22:33:49 +0600

    Simple trie delete

--- Data_Structure/Trie/Trie.py
+++ Data_Structure/Trie/Trie.py
@@ -34,7 +34,21 @@ class Trie:
             current_dict = current_dict.setdefault(letter, {})
         current_dict[_end] = _end
 
+    # Gives the right answer but don't clear the tree structure
+    @staticmethod
+    def simple_delete(trie, word):
+        current_dict = trie
+
+        for letter in word:
+            current_dict = current_dict.get(letter, None)
+
+            if current_dict is None:
+                break
+
+        else:
+            del current_dict[_end]
 
 trie = Trie.make_trie('foo', 'bar', 'baz', 'barz')
 Trie.insert_into_trie(trie, 'foro')
+Trie.delete_from_trie(trie, 'foro')
 print(Trie.in_trie(trie, 'foro'))
